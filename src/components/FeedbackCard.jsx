b7f15edf96d281268cce9cc78aa5ff6f9e88557b
srt1017 <omarfarouq107@gmail.com>
2023-07-22 17:40:30 -0500

    c

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,5 +1,6 @@
 import { quotes } from "../assets";
 
+// eslint-disable-next-line react/prop-types
 const FeedbackCard = ({ content, name, title, img }) => (
   <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
     <img src={quotes} alt="double_quotes" className="w-[42.6px] h-[27.6px] object-contain" />
